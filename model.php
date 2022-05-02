67b943c8c848e55de11aaa20af438194b6278cc0
yen0306 <93121918+yen0306@users.noreply.github.com>
2022-05-02 10:36:30 +0800

    Update model.php

--- model.php
+++ model.php
@@ -1,9 +1,9 @@
 <?php
 require("dbconfig.php");
 
-function isExist($courseName) {  #查看是否已有資料
+function isExist($courseName) {  //查看是否已有資料
     global $db;
-    $sql = "select count(*) from course where courseName = ?";
+    $sql = "select count(*) from course where courseName = ?;";
     $stmt = mysqli_prepare($db, $sql);
     mysqli_stmt_bind_param($stmt, "s", $courseName);
     mysqli_stmt_execute($stmt);
@@ -11,7 +11,17 @@ function isExist($courseName) {  #查看是否已有資料
     $rs = mysqli_fetch_assoc($result);
     return $rs;
 }
-function crawler($courseName) {  #執行爬蟲程式
+function getcid($courseName) {  //取得cid
+    global $db;
+    $sql = "select cid from course where courseName = ?;";
+    $stmt = mysqli_prepare($db, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $courseName);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $rs = mysqli_fetch_assoc($result);
+    return $rs;
+}
+function crawler($courseName) {  //執行爬蟲程式
     exec("python crawler.py $courseName");
 }
 ?>
