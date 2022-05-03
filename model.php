0e94430908f3f88376e38aa4c1f7aee66504c58f
yen0306 <93121918+yen0306@users.noreply.github.com>
2022-05-03 17:16:16 +0800

    Update model.php

--- model.php
+++ model.php
@@ -24,4 +24,22 @@ function getcid($courseName) {  //取得cid
 function crawler($courseName) {  //執行爬蟲程式
     exec("python crawler.py $courseName");
 }
+function getInfo($cid) {  //取得文章所有資訊
+    global $db;
+    $sql = "select title, content, floor, comment from article, comment where article.cid=? and article.aid=comment.aid";
+    $stmt = mysqli_prepare($db, $sql);
+    mysqli_stmt_bind_param($stmt, "i", $cid);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $retArr=array();
+    while ($rs = mysqli_fetch_assoc($result)) {
+        $tArr=array();
+        $tArr['title'] = $rs['title'];
+        $tArr['content'] = $rs['content'];
+        $tArr['floor'] = $rs['floor'];
+        $tArr['comment'] = $rs['comment'];
+        $retArr[] = $tArr;;
+    }
+    return $retArr;
+}
 ?>
